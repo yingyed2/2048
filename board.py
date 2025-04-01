1bbc12261ed10c73a013ccfa7b139fa72722c9a3
Ying Ye Deng <yingye.deng@cooper.edu>
2025-04-01 16:44:25 -0400

    created basic board functions

---
+++ board.py
@@ -0,0 +1,34 @@
+def initBoard():
+
+    emptyRow = [0, 0, 0, 0] # create a list of empty row values
+    board = [] # create an empty list to hold the rows
+
+    # initialize empty board
+    for i in range(4):
+        board.append(emptyRow[:])  # slicing the list to create copies
+
+    return board
+
+"""
+modifying emptyRow would modify every row in the board so,
+[:] ensures each row in the board becomes an independent copy of emptyRow;
+"""
+
+
+def printBoard(board):
+
+    print("\n") # spacing between each board
+
+    for row in board:
+        printedRow = [] # holder variable to be printed
+
+        for cell in row:
+
+            if cell == 0:
+                printedRow.append(" -".rjust(5)) # 0 or empty is represented as - for readability
+
+            else:
+                printedRow.append(str(cell).rjust(5)) # .rjust(n) adds n spaces to the left of the str
+
+        print(" ".join(printedRow)) # concatentates the elements of the each row
+        print("\n") # spacing between each row
