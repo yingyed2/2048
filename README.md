2849f47694e74b8b3f81f712ff621e7a219b9dba
Ying Ye Deng <yingye.deng@cooper.edu>
2025-04-03 12:20:14 -0400

    added new function

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 Project: Recreating the popular game 2048 as part of our final project for ECE-160 Spring 2025
+
+Command(s) to run: python3 main.py
--- board.py
+++ board.py
@@ -1,3 +1,33 @@
+import random # randomly generate 2 or 4 tiles
+
+def placeRandomTile(board):
+
+    emptyPositions = [] # list to store the positions of empty tiles
+    for row in range(4):
+        for col in range(4):
+            if board[row][col] == 0: # checking if position is empty
+                emptyPositions.append((row, col)) # adding empty positions to list
+
+    if len(emptyPositions) == 0:
+        return  # exits the function if there are no empty tiles
+
+    randomPosition = random.choice(emptyPositions) # function from random module to select from empty list
+    row, col = randomPosition # defines the selected position as row and column indices
+
+    randomValue = random.random() # generate a random float between 0 and 1
+    if randomValue < 0.9:
+        tileValue = 2 # 90% to place 2
+    else:
+        tileValue = 4 # 10% to place 4
+
+    board[row][col] = tileValue # places the tileValue into the randomPosition
+
+"""
+utilizes the random module, using the random.choice and random.random functions
+to generate a random value tile onto a random position of the board
+"""
+
+
 def initBoard():
 
     emptyRow = [0, 0, 0, 0] # create a list of empty row values
@@ -6,6 +36,9 @@ def initBoard():
     # initialize empty board
     for i in range(4):
         board.append(emptyRow[:])  # slicing the list to create copies
+
+    placeRandomTile(board)
+    placeRandomTile(board)
 
     return board
 
@@ -22,13 +55,13 @@ def printBoard(board): # board is passed in as an argument
     for row in board:
         printedRow = [] # holder variable to be printed
 
-        for cell in row:
+        for tile in row:
 
-            if cell == 0:
+            if tile == 0:
                 printedRow.append(" -".rjust(5)) # 0 or empty is represented as - for readability
 
             else:
-                printedRow.append(str(cell).rjust(5)) # .rjust(n) adds n spaces to the left of the str
+                printedRow.append(str(tile).rjust(5)) # .rjust(n) adds n spaces to the left of the str
 
         print(" ".join(printedRow)) # concatentates the elements of the each row
         print("\n") # spacing between each row
